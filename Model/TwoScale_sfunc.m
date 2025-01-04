3b3a39052403d7de823c7e17e825abfff8a1937d
mahdiafz <87833288+mahdiafz@users.noreply.github.com>
2025-01-04 16:51:02 +0330

    first

---
+++ Model/TwoScale_sfunc.m
@@ -0,0 +1,160 @@
+function [sys,x0,str,ts,simStateCompliance] = TwoScale_sfunc(t,x,u,flag)
+
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes;
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives(t,x,u);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2,
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4,
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9,
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes
+
+sizes = simsizes;
+
+sizes.NumContStates  = 18;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 18;
+sizes.NumInputs      = 13;
+sizes.DirFeedthrough = 0;
+sizes.NumSampleTimes = 1;   % at least one sample time is needed
+
+sys = simsizes(sizes);
+
+%
+% initialize the initial conditions
+%
+% x0 = [1,0.6,0.7,0.7,1,1,1,1,0.6,25,1];
+x0 = [220,131745,48465920,5,2018,336476,0.4,39464,1.2e4,2,0.5,3.5e-12, 2.85e-14, 2.6e-12, 7e-8, 2.5e-13, 2.5e-12,2.5e-14];
+% x0 = [0,0,0,0,0,0,0,0,0,0,0];
+%
+% str is always an empty matrix
+%
+str = [];
+%
+% initialize the array of sample times
+%
+ts  = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,u)
+dax1t = u(1);
+Dax1t = u(2);
+sf1t = u(3);
+Sf1t = u(4);
+stARt = u(5);
+StARt = u(6);
+I_ext = u(7);
+mu_StARp37 = u(8);
+K_ACTH_StARp37 = u(9);
+k_StARp37 = u(10);
+k_ACORT = u(11);
+k21 = u(12);
+ke = u(13);
+sys = TwoScaleEquations(t,x,dax1t,Dax1t,sf1t,Sf1t,stARt,StARt,I_ext,mu_StARp37,K_ACTH_StARp37,k_StARp37,k_ACORT,k21,ke);
+
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+sys = [];
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u)
+sys = x;
+
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
